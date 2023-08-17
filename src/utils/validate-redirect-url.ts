ed6e5bd0ce83d96190dc5298de7a373b4502d7c5
Markus Ahlstrand <markus@ahlstrand.de>
2023-08-17 12:09:19 +0200

    fix: improve redirect validation

--- src/utils/validate-redirect-url.ts
+++ src/utils/validate-redirect-url.ts
@@ -1,6 +1,11 @@
+import { InvalidRedirectError } from "../errors";
+
 function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\/]/g, "\\$&");
 }
+
+// This splits a url in a hosts part including the protocol and the path
+const urlPattern: RegExp = /^((?:http[s]?:\/\/)?[^\/]+)(\/.*)?$/;
 
 export function validateRedirectUrl(
   allowedUrls: string[],
@@ -10,18 +15,25 @@ export function validateRedirectUrl(
     return;
   }
 
-  const regexes = allowedUrls.map(
-    // This replaces * with .*
-    (allowedUrl) => {
-      const url = new URL(allowedUrl);
-      const path = escapeRegExp(url.pathname.replace(/\/$/, ""));
-      const host = escapeRegExp(url.host).replace(/\\\*/g, ".*");
+  const regexes = allowedUrls.map((allowedUrl) => {
+    // This doesn't work in cloudflare workers for whatever reason
+    // const url = new URL(allowedUrl);
+    const match: RegExpMatchArray | null = allowedUrl.match(urlPattern);
+    if (!match) {
+      throw new Error("Invalid URL");
+    }
 
-      return new RegExp(`^${url.protocol}\/\/${host}${path}$`, "i");
-    },
-  );
+    // This replaces * with .* and escapes any other regexes in the string
+    const host = escapeRegExp(match[1]).replace(/\\\*/g, ".*");
+    // This removes any trailing slahes in the path and escapes any other regexes in the string
+    const path = escapeRegExp(match[2] || "").replace(/\/$/, "");
 
-  if (!regexes.some((regex) => regex.test(redirectUri))) {
-    throw new Error("Invalid redirectUri");
+    return new RegExp(`^${host}${path}$`, "i");
+  });
+
+  const redirectUrlWithoutTrailingSlash = redirectUri.replace(/\/+$/, "");
+
+  if (!regexes.some((regex) => regex.test(redirectUrlWithoutTrailingSlash))) {
+    throw new InvalidRedirectError("Invalid redirectUri");
   }
 }
