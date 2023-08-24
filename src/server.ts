9fb8861d8c5de512260c0b13d69eba6662985e37
Markus Ahlstrand <markus@ahlstrand.de>
2023-08-24 10:56:40 +0200

    fix: format

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const server = {
   async fetch(
     request: Request,
     env: Env,
-    ctx: ExecutionContext
+    ctx: ExecutionContext,
   ): Promise<Response> {
     return app.handle(
       request,
@@ -27,7 +27,7 @@ const server = {
         stateFactory: State.getFactory(env.STATE, env),
         userFactory: User.getFactory(env.USER, env),
       },
-      ctx
+      ctx,
     );
   },
   async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
@@ -36,7 +36,7 @@ const server = {
   async queue(
     batch: MessageBatch<QueueMessage>,
     env: Env,
-    ctx: ExecutionContext
+    ctx: ExecutionContext,
   ) {
     for (const message of batch.messages) {
       const { body } = message;
