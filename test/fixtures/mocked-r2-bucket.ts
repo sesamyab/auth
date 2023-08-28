bcf74a80fb571bbe4f1d460d9b1bbb1997d17a29
Daniel Gent <dan@danielgent.com>
2023-08-28 11:54:39 +0100

    fix: prettier

--- test/fixtures/mocked-r2-bucket.ts
+++ test/fixtures/mocked-r2-bucket.ts
@@ -6,7 +6,7 @@ export function mockedR2Bucket() {
     get: async (path: string) => {
       // It doesn't look like we're using this elsewhere in the codebase... as the R2 bucket definitely is not in the root of the project
       const file = readFileSync(
-        join(__dirname, "../../src/email-templates/output", path)
+        join(__dirname, "../../src/email-templates/output", path),
       );
 
       return {
