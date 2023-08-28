8bb9f11f26754df149d2d9d191670e8e37089ddd
Daniel Gent <dan@danielgent.com>
2023-08-28 11:12:02 +0100

    test: fix fixture so test passes but not a real solution

--- test/fixtures/mocked-r2-bucket.ts
+++ test/fixtures/mocked-r2-bucket.ts
@@ -4,7 +4,10 @@ import { join } from "path";
 export function mockedR2Bucket() {
   return {
     get: async (path: string) => {
-      const file = readFileSync(join(__dirname, "../../src", path));
+      // It doesn't look like we're using this elsewhere in the codebase... as the R2 bucket definitely is not in the root of the project
+      const file = readFileSync(
+        join(__dirname, "../../src/email-templates/output", path)
+      );
 
       return {
         text: async () => {
