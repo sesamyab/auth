b682f8ad7a6080134d131e7c6b0aef7db7e2e16c
Daniel Gent <dan@danielgent.com>
2023-09-08 17:07:15 +0100

    feat: add tenant-id to CORS headers

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -17,11 +17,7 @@ export default async function corsMiddleware<Env>(
   response?.headers.set(headers.accessControlAllowCredentials, "true");
   response?.headers.set(
     headers.accessControlAllowHeaders,
-    "Content-Type, Auth0-Client, Authorization, Range",
-  );
-  response?.headers.set(
-    headers.accessControlAllowHeaders,
-    "Content-Type, Auth0-Client, Authorization, Range",
+    "Content-Type, Auth0-Client, Authorization, Range, tenant-id",
   );
   response?.headers.set(headers.accessControlExposeHeaders, "Content-Range");
 
