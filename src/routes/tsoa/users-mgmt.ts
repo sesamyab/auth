5e21669f0ae63f1d743e726e234a990ff91b3842
Daniel Gent <dan@danielgent.com>
2023-09-08 16:50:11 +0100

    feat: basic list users

--- src/routes/tsoa/users-mgmt.ts
+++ src/routes/tsoa/users-mgmt.ts
@@ -24,6 +24,56 @@ import { User } from "../../types/sql/User";
 // TODO - need security!
 // @Security("oauth2managementApi", [""])
 export class UsersMgmtController extends Controller {
+  @Get("users")
+  public async listUsers(
+    @Request() request: RequestWithContext,
+    @Path("tenantId") tenantId: string,
+    @Header("range") rangeRequest?: string,
+    @Query("filter") filterQuerystring?: string,
+  ): Promise<User[]> {
+    const { ctx } = request;
+
+    const db = getDb(ctx.env);
+
+    let query = db.selectFrom("users").where("users.tenantId", "=", tenantId);
+
+    // TODO - implement this?
+    // if (filterQuerystring) {
+    //   const filter = FilterSchema.parse(JSON.parse(filterQuerystring));
+
+    //   if (filter.q) {
+    //     query = query.where((eb) =>
+    //       eb.or([
+    //         eb("name", "like", `%${filter.q}%`),
+    //         eb("email", "like", `%${filter.q}%`),
+    //       ]),
+    //     );
+    //   }
+    // }
+
+    // const { data, range } = await executeQuery(query, rangeRequest);
+
+    // if (range) {
+    //   this.setHeader(headers.contentRange, range);
+    // }
+
+    // return data.map((user) => ({
+    //   ...user,
+    //   tags: JSON.parse(user.tags || "[]"),
+    // }));
+
+    const dbUsersList = await db
+      .selectFrom("users")
+      .where("users.tenantId", "=", tenantId)
+      .selectAll()
+      .execute();
+
+    return dbUsersList.map((user) => ({
+      ...user,
+      tags: JSON.parse(user.tags || "[]"),
+    }));
+  }
+
   @Get("users/{userId}")
   public async getUser(
     @Request() request: RequestWithContext,
