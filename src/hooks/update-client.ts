37344f9939ad5726b7c416339c91aa45f7fc5071
Markus Ahlstrand <markus@ahlstrand.de>
2023-09-12 16:24:57 +0200

    feat: patch up reset password

--- src/hooks/update-client.ts
+++ src/hooks/update-client.ts
@@ -69,6 +69,8 @@ export async function updateClientInKV(env: Env, applicationId: string) {
     .select(["domain", "dkim_private_key"])
     .execute();
 
+  console.log("got here");
+
   const client: PartialClient = {
     id: application.id,
     name: application.name,
@@ -93,7 +95,11 @@ export async function updateClientInKV(env: Env, applicationId: string) {
     }),
   };
 
-  await env.CLIENTS.put(applicationId, JSON.stringify(client));
+  console.log("applicationId: " + applicationId);
+
+  await env.CLIENTS.put(applicationId, JSON.stringify(client), {});
+
+  console.log("clien: " + JSON.stringify(client));
 
   return client;
 }
