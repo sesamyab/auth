f002f5857f2629bedad7221a9a0e39459146e0a1
Markus Ahlstrand <markus@ahlstrand.de>
2023-09-20 10:49:34 +0200

    fix: review comments

--- integration-test/jwks.spec.ts
+++ integration-test/jwks.spec.ts
@@ -1,6 +1,6 @@
 import { start } from "./server";
 
-describe("ping", () => {
+describe("jwks", () => {
   let worker;
 
   beforeEach(async () => {
@@ -11,31 +11,29 @@ describe("ping", () => {
     worker.stop();
   });
 
-  describe("jwks", () => {
-    it("should return an empty list in an empty system", async () => {
-      const response = await worker.fetch("/.well-known/jwks.json");
+  it("should return an empty list in an empty system", async () => {
+    const response = await worker.fetch("/.well-known/jwks.json");
 
-      expect(response.status).toBe(200);
+    expect(response.status).toBe(200);
 
-      const body = await response.json();
-      expect(body).toEqual({ keys: [] });
-    });
-
-    it("should create a new rsa-key and return it", async () => {
-      await worker.fetch("/create-key", {
-        method: "POST",
-      });
-
-      const response = await worker.fetch("/.well-known/jwks.json");
-
-      expect(response.status).toBe(200);
-
-      const body = await response.json();
-      expect(body.keys.length).toBe(1);
-    });
+    const body = await response.json();
+    expect(body).toEqual({ keys: [] });
   });
 
-  it("openid-configuration", async () => {
+  it("should create a new rsa-key and return it", async () => {
+    await worker.fetch("/create-key", {
+      method: "POST",
+    });
+
+    const response = await worker.fetch("/.well-known/jwks.json");
+
+    expect(response.status).toBe(200);
+
+    const body = await response.json();
+    expect(body.keys.length).toBe(1);
+  });
+
+  it("should return a openid-configuration with the current issues", async () => {
     const response = await worker.fetch("/.well-known/openid-configuration");
 
     expect(response.status).toBe(200);
