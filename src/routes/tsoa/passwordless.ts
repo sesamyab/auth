53479feabb0b80d336a1d1e48dc1c3d2415729e9
Markus Ahlstrand <markus@ahlstrand.de>
2023-09-20 13:21:14 +0200

    feat: magic link support

--- src/routes/tsoa/passwordless.ts
+++ src/routes/tsoa/passwordless.ts
@@ -1,9 +1,21 @@
 // src/users/usersController.ts
-import { Body, Controller, Post, Request, Route, Tags } from "@tsoa/runtime";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Request,
+  Route,
+  Tags,
+} from "@tsoa/runtime";
 import { RequestWithContext } from "../../types/RequestWithContext";
 import { getClient } from "../../services/clients";
 import { AuthParams } from "../../types/AuthParams";
 import { sendCode } from "../../controllers/email";
+import { generateAuthResponse } from "../../helpers/generate-auth-response";
+import { applyTokenResponse } from "../../helpers/apply-token-response";
+import { base64ToHex } from "../../utils/base64";
 
 export interface PasswordlessOptions {
   client_id: string;
@@ -53,5 +65,65 @@ export class PasswordlessController extends Controller {
     await sendCode(env, client, body.email, code);
 
     return "ok";
+  }
+
+  /*
+          scope: openid profile email
+response_type: token id_token
+redirect_uri: https://login2-4pcueclm6.vercel.sesamy.dev/callback
+audience: https://sesamy.com
+state: ACRvQXbA_AbvXJGiorgmDAjakL3jXjYl
+nonce: oO0sJ_sq127hoMG8pM.5sjgk.IZQ8oc_
+verification_code: 625452
+connection: email
+client_id: 0N0wUHXFl0TMTY2L9aDJYvwX7Xy84HkW
+email: dan+456@sesamy.com
+     */
+
+  @Get("verify_redirect")
+  public async verifyRedirect(
+    @Request() request: RequestWithContext,
+    @Query("scope") scope: string,
+    @Query("response_type") response_type: string,
+    @Query("redirect_uri") redirect_uri: string,
+    @Query("audience") audience: string,
+    @Query("state") state: string,
+    @Query("nonce") nonce: string,
+    @Query("verification_code") verification_code: string,
+    @Query("connection") connection: string,
+    @Query("client_id") client_id: string,
+    @Query("email") email: string,
+  ): Promise<string> {
+    const { env } = request.ctx;
+
+    const client = await getClient(env, client_id);
+    if (!client) {
+      throw new Error("Client not found");
+    }
+
+    const stateInstance = env.stateFactory.getInstanceById(base64ToHex(state));
+    const authParams = await stateInstance.getState.query();
+
+    const user = env.userFactory.getInstanceByName(
+      `${client.tenant_id}|${email}`,
+    );
+    await user.validateAuthenticationCode.mutate({
+      code: verification_code,
+      email,
+      tenantId: client.tenant_id,
+    });
+
+    const tokenResponse = generateAuthResponse(env, client, email, {
+      scope,
+      response_type,
+      redirect_uri,
+      audience,
+      state,
+      nonce,
+      connection,
+      client_id,
+    });
+
+    return applyTokenResponse(this, tokenResponse, authParams);
   }
 }
