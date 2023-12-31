eda0f806672fcdf0920eae67591c9901638edb7b
Daniel Gent <dan@sesamy.com>
2023-12-31 15:18:44 +0000

    fix: remove nullable tenant_id

--- src/types/User.ts
+++ src/types/User.ts
@@ -8,7 +8,6 @@ export interface BaseUser {
   picture?: string;
   locale?: string;
   linked_to_id?: string;
-  tenant_id?: string;
   profileData?: string;
   // TBD - adding this here as optional for when destructing id_token and updating
   email_verified?: boolean;
