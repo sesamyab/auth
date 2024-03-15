eaa48f7eba47c7f5a4eb1c2d7ab48920f0fd649a
Daniel Gent <dan@sesamy.com>
2024-03-15 13:50:20 +0200

    fix: ignore all unverified accounts

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -54,21 +54,15 @@ export async function getPrimaryUserByEmail({
   tenant_id,
   email,
 }: GetPrimaryUserByEmailParams): Promise<User | undefined> {
-  const { users: usersWithUnverifiedPasswordAccounts } = await userAdapter.list(
-    tenant_id,
-    {
-      page: 0,
-      per_page: 10,
-      include_totals: false,
-      q: `email:${email}`,
-    },
-  );
+  const { users: usersPossiblyUnverified } = await userAdapter.list(tenant_id, {
+    page: 0,
+    per_page: 10,
+    include_totals: false,
+    q: `email:${email}`,
+  });
 
   // filter out unverified accounts
-  const users = usersWithUnverifiedPasswordAccounts.filter(
-    // maybe we should do this for all providers
-    (user) => !(user.provider === "auth2" && !user.email_verified),
-  );
+  const users = usersPossiblyUnverified.filter((user) => user.email_verified);
 
   if (users.length === 0) {
     return;
