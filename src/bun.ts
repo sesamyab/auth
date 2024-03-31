9078c76b394c1c626a8015c0c54076de5c050da6
Markus Ahlstrand <markus@ahlstrand.de>
2024-03-31 12:01:43 +0200

    fix: bun database

--- src/bun.ts
+++ src/bun.ts
@@ -1,9 +1,15 @@
 import { serveStatic } from "hono/bun";
+import { Database } from "bun:sqlite";
+import { drizzle } from "drizzle-orm/bun-sqlite";
 import app from "../src/app";
 import { oAuth2ClientFactory } from "../src/services/oauth2-client";
-import createAdapters from "./adapters/drizzle-mysql";
+import createAdapters from "./adapters/drizzle-sqlite";
 import createEmailAdapter from "./adapters/email";
-import { db } from "./services/drizzle-sqlite";
+import * as schema from "../drizzle-sqlite/schema";
+import { DrizzleSQLiteDatabase } from "./services/drizzle-sqlite";
+
+const sqlite = new Database("db.sqlite");
+const db = drizzle(sqlite, { schema });
 
 app.use("/static/*", serveStatic({ root: "./" }));
 
@@ -14,7 +20,8 @@ const server = {
       oauth2ClientFactory: { create: oAuth2ClientFactory },
       data: {
         ...createEmailAdapter(),
-        ...createAdapters(db),
+        // TODO: this doesn't really matter as they both work the same, but.. it would be nice to have a base type for this here
+        ...createAdapters(db as unknown as DrizzleSQLiteDatabase),
       },
     });
   },
