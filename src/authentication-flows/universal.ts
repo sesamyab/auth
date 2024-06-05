61b8271660f7880aa3b45a291a8fe8f184b62df5
Daniel Gent <dan@sesamy.com>
2024-06-05 14:12:57 +0100

    feat: send a code email if email_hint

--- src/authentication-flows/universal.ts
+++ src/authentication-flows/universal.ts
@@ -5,6 +5,10 @@ import { UNIVERSAL_AUTH_SESSION_EXPIRES_IN_SECONDS } from "../constants";
 import { nanoid } from "nanoid";
 import { UniversalLoginSession } from "../adapters/interfaces/UniversalLoginSession";
 import { getClient } from "../services/clients";
+import generateOTP from "../utils/otp";
+import { getSendParamFromAuth0ClientHeader } from "../utils/getSendParamFromAuth0ClientHeader";
+import { waitUntil } from "../utils/wait-until";
+import { sendCode, sendLink } from "../controllers/email";
 
 interface UniversalAuthParams {
   ctx: Context<{ Bindings: Env; Variables: Var }>;
@@ -40,7 +44,81 @@ export async function universalAuth({
   await ctx.env.data.universalLoginSessions.create(session);
 
   if (emailHint) {
-    // TODO - need to send a code email here...
+    const { env } = ctx;
+    // TODO - extract this out as a helper. Duplciated on routes as well
+    // need this code again for when user hits "enter a code instead" button on the password entry page
+    const code = generateOTP();
+
+    const {
+      audience,
+      code_challenge_method,
+      code_challenge,
+      username,
+      vendor_id,
+      ...otpAuthParams
+    } = session.authParams;
+
+    // duplicated in many places under many names
+    const OTP_EXPIRATION_TIME = 30 * 60 * 1000;
+
+    await env.data.OTP.create({
+      id: nanoid(),
+      code,
+      // should we be doing any cleaning here?
+      email: emailHint,
+      client_id: session.authParams.client_id,
+      send: "code",
+      authParams: otpAuthParams,
+      tenant_id: client.tenant_id,
+      created_at: new Date(),
+      expires_at: new Date(Date.now() + OTP_EXPIRATION_TIME),
+    });
+
+    // request.ctx.set("log", `Code: ${code}`);
+
+    const sendType = getSendParamFromAuth0ClientHeader(session.auth0Client);
+
+    // TODO - read cookie here to decide if go to password entry step?
+
+    if (sendType === "link") {
+      const magicLink = new URL(env.ISSUER);
+      magicLink.pathname = "passwordless/verify_redirect";
+      if (session.authParams.scope) {
+        magicLink.searchParams.set("scope", session.authParams.scope);
+      }
+      if (session.authParams.response_type) {
+        magicLink.searchParams.set(
+          "response_type",
+          session.authParams.response_type,
+        );
+      }
+      if (session.authParams.redirect_uri) {
+        magicLink.searchParams.set(
+          "redirect_uri",
+          session.authParams.redirect_uri,
+        );
+      }
+      if (session.authParams.audience) {
+        magicLink.searchParams.set("audience", session.authParams.audience);
+      }
+      if (session.authParams.state) {
+        magicLink.searchParams.set("state", session.authParams.state);
+      }
+      if (session.authParams.nonce) {
+        magicLink.searchParams.set("nonce", session.authParams.nonce);
+      }
+
+      magicLink.searchParams.set("connection", "email");
+      magicLink.searchParams.set("client_id", session.authParams.client_id);
+      magicLink.searchParams.set("email", emailHint);
+      magicLink.searchParams.set("verification_code", code);
+      magicLink.searchParams.set("nonce", "nonce");
+
+      waitUntil(ctx, sendLink(env, client, emailHint, code, magicLink.href));
+    } else {
+      waitUntil(ctx, sendCode(env, client, emailHint, code));
+    }
+
     return ctx.redirect(`/u/enter-code?state=${session.id}`);
   }
 
