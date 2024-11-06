daff308532d2e25bbcb2e17c0c5fc2d6adc7d491
Markus Ahlstrand <markus@ahlstrand.de>
2024-11-07 00:10:43 +0100

    fix: add a screen for incognito links

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,3 @@
-import type { FC } from "hono/jsx";
 import { VendorSettings } from "@authhero/adapter-interfaces";
 import AppLogo from "./AppLogo";
 import i18next from "i18next";
@@ -70,28 +69,6 @@ const Layout = ({
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <style>{globalDocStyle(vendorSettings)}</style>
-        {/* <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
-          color="#000000"
-        /> */}
-        {/* <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32-light.png"
-        /> */}
-        {/* <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16-light.png"
-        /> */}
         <meta name="theme-color" content="#000000" />
       </head>
 
