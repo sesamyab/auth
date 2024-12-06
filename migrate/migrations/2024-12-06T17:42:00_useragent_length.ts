be08b4833ec0136a7dca8f3784905f2ba38fc13e
Markus Ahlstrand <markus@ahlstrand.de>
2024-12-06 18:09:22 +0100

    fix: make useragent longer

--- migrate/migrations/2024-12-06T17:42:00_useragent_length.ts
+++ migrate/migrations/2024-12-06T17:42:00_useragent_length.ts
@@ -3,17 +3,17 @@ import { Database } from "@authhero/kysely-adapter";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function up(db: Kysely<Database>): Promise<void> {
-  // await db.schema
-  //   .alterTable("logins")
-  // .addColumn("ip", "varchar(255)")
-  // .addColumn("useragent", "varchar(255)")
-  // .execute();
+  await db.schema
+    .alterTable("logins")
+    .dropColumn("useragent")
+    .addColumn("useragent", "varchar(512)")
+    .execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
   await db.schema
     .alterTable("logins")
-    .dropColumn("ip")
     .dropColumn("useragent")
+    .addColumn("useragent", "varchar(255)")
     .execute();
 }
