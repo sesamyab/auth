15daa3614ebdbe9be3c05da062501eea4688153e
Markus Ahlstrand <markus@ahlstrand.de>
2024-12-10 16:48:42 +0100

    fix: review comment

--- src/strategies/google-oauth2.ts
+++ src/strategies/google-oauth2.ts
@@ -47,12 +47,7 @@ export async function validateAuthorizationCodeAndGetUser(
   const { options } = connection;
 
   if (!options?.client_id || !options.client_secret || !code_verifier) {
-    throw new Error(
-      "Missing required Google authentication parameters: " +
-        JSON.stringify(options) +
-        " " +
-        code_verifier,
-    );
+    throw new Error("Missing required authentication parameters");
   }
 
   const google = new Google(
