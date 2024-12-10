d229918cfe5828fe9f0000aa2f5beedbd5e11392
Markus Ahlstrand <markus@ahlstrand.de>
2024-12-10 16:47:16 +0100

    fix: commented out code

---
+++ src/strategies/membercare.ts
@@ -0,0 +1,129 @@
+import { OAuth2Client } from "arctic";
+import { Context } from "hono";
+import { Connection } from "authhero";
+import { nanoid } from "nanoid";
+import { Env, Var } from "../types";
+import { parseJWT } from "oslo/jwt";
+// import { idTokenSchema } from "../types/IdToken";
+// import { url } from "inspector";
+import { z } from "zod";
+
+export async function getRedirect(
+  ctx: Context<{ Bindings: Env; Variables: Var }>,
+  connection: Connection,
+) {
+  const { options } = connection;
+
+  if (
+    !options?.client_id ||
+    !options.client_secret ||
+    !options.authorization_endpoint
+  ) {
+    throw new Error("Missing required authentication parameters");
+  }
+
+  const oauth2 = new OAuth2Client(
+    options.client_id,
+    options.client_secret,
+    `${ctx.env.ISSUER}callback`,
+  );
+
+  const code = nanoid();
+
+  const authorizationUrl = oauth2.createAuthorizationURL(
+    options.authorization_endpoint,
+    code,
+    ["openid"],
+  );
+
+  return {
+    redirectUrl: authorizationUrl.href,
+    code,
+  };
+}
+
+export async function validateAuthorizationCodeAndGetUser(
+  ctx: Context<{ Bindings: Env; Variables: Var }>,
+  connection: Connection,
+  code: string,
+) {
+  const { options } = connection;
+
+  if (
+    !options?.client_id ||
+    !options.client_secret ||
+    !options.token_endpoint ||
+    !options.userinfo_endpoint ||
+    !options.app_secret
+  ) {
+    throw new Error("Missing required authentication parameters");
+  }
+
+  const oauth2 = new OAuth2Client(
+    options.client_id,
+    options.client_secret,
+    `${ctx.env.ISSUER}callback`,
+  );
+
+  const tokens = await oauth2.validateAuthorizationCode(
+    options.token_endpoint,
+    code,
+    null,
+  );
+
+  const accessToken = parseJWT(tokens.accessToken());
+  if (
+    !accessToken ||
+    !("sub" in accessToken.payload) ||
+    typeof accessToken.payload.sub !== "string"
+  ) {
+    throw new Error("Invalid access token");
+  }
+
+  // The email isn't available in the tokens so we need to make an additional request to the api to get it
+  // First fetch a token from the userinfo endpoint
+  const [clientApiKey, personToImpersonate] = options.app_secret.split("|");
+  const tokenUrl = new URL(options.userinfo_endpoint);
+  tokenUrl.pathname = "/api/v1/token";
+  tokenUrl.searchParams.set("clientApiKey", clientApiKey);
+  tokenUrl.searchParams.set("personToImpersonate", personToImpersonate);
+
+  const tokenResponse = await fetch(options.userinfo_endpoint, {
+    cf: {
+      // Add cache headers for cloudflare here
+    },
+  });
+  if (!tokenResponse.ok) {
+    throw new Error("Failed to fetch token");
+  }
+  const { value } = z
+    .object({
+      value: z.string(),
+      expiration: z.string(),
+    })
+    .parse(await tokenResponse.json());
+
+  const userInfoUrl = new URL(options.userinfo_endpoint);
+  userInfoUrl.pathname = `/api/v1/debtors/${accessToken.payload.sub}`;
+  const debtorResponse = await fetch(userInfoUrl.href, {
+    headers: {
+      token: value,
+    },
+    cf: {
+      // Add cache headers for cloudflare here
+    },
+  });
+
+  if (!debtorResponse.ok) {
+    throw new Error("Failed to fetch user info");
+  }
+
+  throw new Error("Not implemented");
+
+  // const debtor = z.ParseStatus();
+
+  // return {
+  //   id: payload.sub,
+  //   email: payload.email,
+  // };
+}
