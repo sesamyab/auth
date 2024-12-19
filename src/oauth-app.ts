dceae7715332bd37503d5a4480b2c725a3b02bc7
Markus Ahlstrand <markus@ahlstrand.es>
2024-12-19 15:18:10 +0100

    fix: add magic links back

--- src/oauth-app.ts
+++ src/oauth-app.ts
@@ -6,6 +6,7 @@ import { loginRoutes } from "./routes/universal-login/routes";
 import { authorizeRoutes } from "./routes/oauth2/authorize";
 import { callbackRoutes } from "./routes/oauth2/callback";
 import { authenticateRoutes } from "./routes/oauth2/authenticate";
+import { passwordlessRoutes } from "./routes/oauth2/passwordless";
 
 export default function create(params: CreateAuthParams) {
   const app = new OpenAPIHono<{ Bindings: Env; Variables: Var }>();
@@ -19,7 +20,8 @@ export default function create(params: CreateAuthParams) {
     .route("/u", loginRoutes)
     .route("/authorize", authorizeRoutes)
     .route("/callback", callbackRoutes)
-    .route("/co/authenticate", authenticateRoutes);
+    .route("/co/authenticate", authenticateRoutes)
+    .route("/passwordless", passwordlessRoutes);
 
   oauthApp.doc("/spec", {
     openapi: "3.0.0",
